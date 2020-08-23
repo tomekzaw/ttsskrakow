29b721c57cbd51aefce6e358ba0147a679dddaf2
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2020-08-23 10:42:07 +0200

    rename position to center

--- frontend/src/VehiclesMap.js
+++ frontend/src/VehiclesMap.js
@@ -5,14 +5,14 @@ import ActiveVehiclePolyline from './ActiveVehiclePolyline.js'
 import './VehiclesMap.css'
 
 export default function VehiclesMap({ vehicles, setActiveVehicle, activeVehicleData }) {
-  const position = [50.04, 19.96]
+  const center = [50.04, 19.96]
   const zoom = 12
 
   function handleClick() {
     setActiveVehicle()
   }
 
-  return <Map center={position} zoom={zoom} onClick={handleClick}>
+  return <Map center={center} zoom={zoom} onClick={handleClick}>
     <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
         url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
