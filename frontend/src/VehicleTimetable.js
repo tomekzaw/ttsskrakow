78b816d4da935182d44251919b59a18dcf926624
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2020-08-23 11:47:10 +0200

    remove >>>

--- frontend/src/VehicleTimetable.js
+++ frontend/src/VehicleTimetable.js
@@ -5,7 +5,7 @@ export default function VehicleTimetable({ departures }) {
   return <table class="departures">
     {departures.map(departure => <tr class={"departure--" + departure.status.toLowerCase()}>
       <td className="departure__time">
-        {departure.status === "STOPPING" ? ">>>" : departure.time}
+        {departure.time}
       </td>
       <td className="departure__status">
         <img src={process.env.PUBLIC_URL + '/stop_' + departure.status.toLowerCase() + '.svg'} alt={departure.status} className="departure__icon" />
