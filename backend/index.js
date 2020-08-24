e35e8efd2e54acbb7497af3a021c7eec52a18c5f
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2020-08-24 21:56:16 +0200

    move server to backend directory

--- backend/index.js
+++ backend/index.js
@@ -69,7 +69,7 @@ app.get('/api/timetable', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/frontend/build/index.html'))
+  res.sendFile(path.join(__dirname + '../frontend/build/index.html'))
 })
 
 const port = process.env.PORT || 5000
